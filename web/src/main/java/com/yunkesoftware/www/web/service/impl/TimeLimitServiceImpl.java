362da3092f7697c99cc0c7e4159ee4543e357022
yy710 <171254868@qq.com>
2026-05-20 21:44:36 +0800

    Update integral time limit window

--- web/src/main/java/com/yunkesoftware/www/web/service/impl/TimeLimitServiceImpl.java
+++ web/src/main/java/com/yunkesoftware/www/web/service/impl/TimeLimitServiceImpl.java
@@ -14,12 +14,12 @@ public class TimeLimitServiceImpl implements TimeLimitService {
     public void checkTimeLimit() {
         // 3月1日00:01-5月20日23:00
         LocalDateTime nowTime = LocalDateTime.now();
-//        // 2026-03-01 00:01:00 2026-05-20 23:00:00
-//        LocalDateTime startTime = LocalDateTime.of(2026, 3, 1, 0, 1);
-//        if (nowTime.isBefore(startTime)) {
-//            throw new YunKeException(ExceptionEnum.FAIL, "获得积分开始时间为：2026-03-01 00:01:00");
-//        }
-        LocalDateTime endTime = LocalDateTime.of(2026, 5, 20, 23, 0);
+        // 2026-03-01 00:01:00 2026-05-20 23:00:00
+        LocalDateTime startTime = LocalDateTime.of(2026, 5, 21, 0, 1);
+        if (nowTime.isBefore(startTime)) {
+            throw new YunKeException(ExceptionEnum.FAIL, "获得积分开始时间为：2026-03-01 00:01:00");
+        }
+        LocalDateTime endTime = LocalDateTime.of(2026, 5, 31, 23, 59);
         if (nowTime.isAfter(endTime)) {
             throw new YunKeException(ExceptionEnum.FAIL, "获得积分结束时间为：2026-05-20 23:00:00");
         }
